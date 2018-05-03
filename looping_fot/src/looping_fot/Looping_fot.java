37a3d83490454bc3c42feccc08cd684602bb94a1
Julianad28 <Julianad2807@gmail.com>
2018-05-03 14:16:15 +0700

    Baru dari Juli

---
+++ looping_fot/src/looping_fot/Looping_fot.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping_fot;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GeeksFarm BSD
+ */
+public class Looping_fot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama,nilai;
+        Scanner inputan = new Scanner (System.in);
+        
+        /*
+        for (int i=0;i<5;i++)
+        {
+        System.out.print(i);
+        }
+        */
+        
+        /*for (int i=0;i<5;i++)
+        {
+        System.out.print("Masukkan Nama Siswa : ");
+        nama=inputan.nextLine();
+        
+        System.out.print("Masukkan nilai : ");
+        nilai=inputan.nextLine();
+        
+        System.out.println("Nilai milik siswa ke-"+i+" dengan nama "+nama+" : "+nilai+);
+        
+        */
+        for (int i=0;i<5;i++)
+        {
+            for (int j=0;j<5;i++)
+            
+            {
+                System.out.print("*");
+                
+            }
+            System.out.println(); 
+        }}}                
+        
+    
+    
+
