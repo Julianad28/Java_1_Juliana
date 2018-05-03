4e5be890310ea3fe20da6afb24b833090c1ee91e
Julianad28 <Julianad2807@gmail.com>
2018-05-03 14:27:13 +0700

    Yang barunya

---
+++ tugas7/src/tugas7/Tugas7.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GeeksFarm BSD
+ */
+public class Tugas7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama,nama_kota,jk;
+        Scanner inputan = new Scanner(System.in);
+        System.out.println("............");
+       
+        for(int i=1;i<4;i++)
+        {
+            System.out.println("data peserta ke-"+i);
+            
+            System.out.print("masukan nama peserta : ");
+            nama=inputan.nextLine();
+            
+            System.out.print("masuka kode kota [B/D/E] : ");
+            nama_kota=inputan.nextLine();
+            
+            System.out.print("masukan jenis kelamin [L/P] : ");
+            jk=inputan.nextLine();
+            
+            System.out.print(nama);
+            
+            switch (nama_kota){
+                case "B":
+                System.out.print("berasal dari jakarta");
+                break;
+                case "D":
+                System.out.print("berasal dari bandung");
+                break;
+                case "E":
+                System.out.print("berasal dari cirebon");
+                break; 
+                default:
+                System.out.print("berasal dari kota tidak tersedia");
+                break;
+    }
+            switch (jk){
+            case "P":
+                System.out.print("dan berjenis kelamin perempuan");
+                break;
+            case "L":
+                System.out.print("dan berjenis kelamin laki-laki");
+                break;
+            default:
+                System.out.print("dan jenis kelamin anda tidak terdeteksi");
+                break;
+           
+            }
+            System.out.println();
+          
+                }
+  }
+        }
+    
+    
+
